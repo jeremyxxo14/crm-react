d498eef1a8e37d127bb1dd03df4842eb0fc450fc
Jeremy Muñoz <jeremyxxo@gmail.com>
2023-12-07 01:26:10 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='text-6xl font-bold'>Crm React - React Rputer DOM</h1>
+    </>
+  )
+}
+
+export default App
